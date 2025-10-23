de855bb5ca2ae139b4b31fcebe8ffa9f7240a570
Toa <toateachestech@gmail.com>
2025-10-23 15:37:22 -0600

    Was originally Yahtzee but decided to just make generic die roller

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-// Name: Toa Pita
-// Description: Handle die rolls
-
-function toggleDieHold(id){
-    let currentState = document.getElementById(id).getAttribute("data-active");
-    let newState = currentState==="true"?false:true;
-    document.getElementById(id).setAttribute("data-active",newState);
-    if(newState){
-        document.getElementById(id).classList.remove("held")
-    }else{
-        document.getElementById(id).classList.add("held")
-    }
-}
 
 
